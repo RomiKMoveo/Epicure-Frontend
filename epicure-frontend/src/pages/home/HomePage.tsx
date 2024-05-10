34f3ab12a5c5915bd3549f12885aebff8a027af4
romi kamin <romik@moveo.co.il>
2024-05-10 15:17:16 +0300

    creating structure (including the homePage and related components) and importing basic typography and style to the homePage

---
+++ epicure-frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,31 @@
+import "./HomePage.scss";
+// import {
+//   AboutSection,
+//   ChefOfTehWeekSection,
+//   DishSection,
+//   RestSection,
+//   SignatureSection,
+//   Hero, 
+//   OrderSuccess
+// } from "../../components";
+
+
+
+const HomePage: React.FC = () => {
+
+  return (
+    <>
+    <h1> hi</h1>
+      {/* <div onClick={sendCloseNavbar}>
+         <Header />
+            <Hero />
+            <IconsMeaning />
+            <Chef />
+            <About />
+            <Footer />
+      </div> */}
+    </>
+  );
+};
+
+export default HomePage;
