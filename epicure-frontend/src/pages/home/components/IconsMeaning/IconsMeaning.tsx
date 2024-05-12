323691c59fe7f1e6ec70923ed8c0c00932be2d53
romi kamin <romik@moveo.co.il>
2024-05-12 17:13:32 +0300

    set a separate component for iconMeaningItem

--- epicure-frontend/src/pages/home/components/IconsMeaning/IconsMeaning.tsx
+++ epicure-frontend/src/pages/home/components/IconsMeaning/IconsMeaning.tsx
@@ -2,8 +2,9 @@ import "./IconsMeaning.scss";
 import spicyIcon from "../../../../assets/icons/ spicy-icon.svg";
 import vegetarianIcon from "../../../../assets/icons/ vegetarian-icon.svg";
 import veganIcon from "../../../../assets/icons/vegan-icon.svg";
+import IconMeaningItem from "./IconMeaningItem/IconsMeaningItem";
 
-const HomePage: React.FC = () => {
+const iconsMeaning: React.FC = () => {
   const iconsMeaningIcons = [
     { icon: spicyIcon, alt: "spicy", caption: "Spicy" },
     { icon: vegetarianIcon, alt: "vegetarian", caption: "Vegetarian" },
@@ -14,16 +15,13 @@ const HomePage: React.FC = () => {
       <section className="icons-meaning-container">
         <div className="icons-meaning-title ">THE MEANING OF OUR ICONS:</div>
         <ul className="icons-list-container">
-            {iconsMeaningIcons.map(({ icon, alt, caption }) => (
-              <figure key={alt} className="icon-meaning-item">
-                <img src={icon} alt={alt} className="icon-img" />
-                <figcaption className="icon-caption">{caption}</figcaption>
-              </figure>
-            ))}
+          {iconsMeaningIcons.map(({ icon, alt, caption }) => (
+            <IconMeaningItem key={alt} icon={icon} alt={alt} caption={caption} />
+          ))}
         </ul>
       </section>
     </>
   );
 };
 
-export default HomePage;
+export default iconsMeaning;
